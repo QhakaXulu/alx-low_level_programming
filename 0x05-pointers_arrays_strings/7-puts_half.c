1864f31848005d362725751f2295d2ea44a37f21
QhakaXulu <phumlagodide@gmail.com>
2023-02-28 04:17:50 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 
 /**
  * puts_half - Prints half of a string
- * @str: The string to print 
+ * @str: The string to print
  * Return: void
  */
 
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-	k = j / 2 ;
+	k = j / 2;
 	}
 
 	for (; k < j ; k++)
